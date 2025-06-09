9fa6d805401e4c3a54c2e56df6ff3cceebfc003e
shivadarshini R <103628098+shivadarshinir@users.noreply.github.com>
2025-06-09 07:36:23 -0700

    Add files via upload

---
+++ prog_7_day.py
@@ -0,0 +1,10 @@
+num = int(input("enters the number: "))
+if num <=30:
+    print("the value is in bettwen 0 to 30",num )
+elif num <= 60:
+    print ("the value is in bettwen 31 to 60")
+elif num <= 100:
+     print ("the value is in bettwen 61 to 100")
+else:
+     print("ENTER VALU IS ABOVE THE RANGE",num)
+
